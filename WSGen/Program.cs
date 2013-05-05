75fecfd6cfa519ece7c75af6bd6e0d6a0380a756
yariki <iyariki.ya@gmail.com>
2013-05-05 10:06:23 +0300

    updated: - added project which will help in generating Version file and updating setup project;

---
+++ WSGen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace WSGen
+{
+    class Program
+    {
+        private const string BuildNumberFilename = "wsui.number";
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Source dir argument is absent.");
+                return;
+            }
+            string source = args[0];
+            string workingDir = Assembly.GetAssembly(typeof (Program)).Location;
+            workingDir = workingDir.Substring(0, workingDir.LastIndexOf('/'));
+            string filePath = workingDir + BuildNumberFilename;
+
+
+        }
+
+
+
+    }
+}
