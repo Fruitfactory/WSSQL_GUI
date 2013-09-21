11f2f03beb43467fc5dc5d85d2a1afdd040b36a5
Yariki <iyariki.ya@gmail.com>
2013-09-21 14:08:47 +0300

    added missing file

---
+++ WSUI.Module/Service/DoubleClickBehabior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WSUI.Module.Service
+{
+    public static class DoubleClickBehabior
+    {
+         
+        public static readonly DependencyProperty DoubleClickHandlerProperty =
+            DependencyProperty.RegisterAttached("DoubleClickHandler", typeof (ICommand), typeof (DoubleClickBehabior), new UIPropertyMetadata(DoubleClickBehabior.DoubleClickHandle));
+
+        public static void SetDoubleClickHandler(DependencyObject target, ICommand value)
+        {
+            target.SetValue(DoubleClickBehabior.DoubleClickHandlerProperty,value);
+        }
+
+        private static void DoubleClickHandle(DependencyObject target, DependencyPropertyChangedEventArgs args)
+        {
+            Control fe = target as Control;
+            if(fe == null)
+                return;
+            if (args.NewValue != null && args.OldValue == null)
+            {
+                fe.MouseDoubleClick += FeOnMouseDoubleClick;
+            }
+            else if (args.NewValue == null && args.OldValue != null)
+            {
+                fe.MouseDoubleClick -= FeOnMouseDoubleClick;
+            }
+
+        }
+
+        private static void FeOnMouseDoubleClick(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            FrameworkElement element = sender as FrameworkElement;
+            ICommand command = (ICommand) element.GetValue(DoubleClickBehabior.DoubleClickHandlerProperty);
+            if (command == null)
+                return;
+            command.Execute(mouseButtonEventArgs);
+        }
+    }
+}
