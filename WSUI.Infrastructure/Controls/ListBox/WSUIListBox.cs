44728b576f3f97a732be5e21733da0ab4f044941
yariki <iyariki.ya@gmail.com>
2013-10-26 19:03:05 +0300

    updated:
    - added our one listbox control;

---
+++ WSUI.Infrastructure/Controls/ListBox/WSUIListBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WSUI.Infrastructure.Helpers.Extensions;
+
+namespace WSUI.Infrastructure.Controls.ListBox
+{
+    public class WSUIListBox : System.Windows.Controls.ListBox
+    {
+        public WSUIListBox()
+        {
+            DefaultStyleKey = typeof(System.Windows.Controls.ListBox);
+        }
+
+        public override void EndInit()
+        {
+            base.EndInit();
+            var listBoxItem = this.GetParent<ListBoxItem>();
+            if (listBoxItem == null)
+                return;
+            DataTrigger dt = new DataTrigger();
+            var binding = new Binding("IsSelected");
+            binding.Source = listBoxItem;
+            dt.Binding = binding;
+            dt.Value = false;
+            dt.Setters.Add(new Setter(SelectedIndexProperty, -1));
+            this.Triggers.Add(dt);
+        }
+    }
+}
---
+++ WSUI.Infrastructure/Helpers/Extensions/UIExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WSUI.Infrastructure.Helpers.Extensions
+{
+    public static class UIExtensions
+    {
+        public static T GetParent<T>(this DependencyObject obj) where T : class
+        {
+            try
+            {
+                var parent = VisualTreeHelper.GetParent(obj);
+                if (parent == null) return default(T);
+                var result = parent as T;
+                return result ?? parent.GetParent<T>();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        public static DependencyObject GetParent(this DependencyObject obj, Type type)
+        {
+            var parent = VisualTreeHelper.GetParent(obj);
+            if (parent == null) return null;
+            var result = parent.GetType() == type;
+            return result ? parent : parent.GetParent(type);
+        }
+
+        public static bool IsDisconnected(this DependencyObject obj)
+        {
+            var parent = VisualTreeHelper.GetParent(obj);
+            if (parent == Application.Current.MainWindow)
+                return false;
+            if (parent == null)
+                return true;
+            return parent.IsDisconnected();
+        }
+    }
+}
