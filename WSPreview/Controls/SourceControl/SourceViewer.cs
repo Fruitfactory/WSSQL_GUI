6e6673833d736bd837ea331dd9a930b1cacd28d5
yariki <iyariki.ya@gmail.com>
2013-11-30 15:31:39 +0200

    updated: - added new control for source preview;

--- WSPreview/Controls/SourceControl/SourceViewer.cs
+++ WSPreview/Controls/SourceControl/SourceViewer.cs
@@ -1,7 +1,13 @@
 ﻿using System.IO;
+using System.Net;
 using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using Microsoft.Office.Interop.Outlook;
 using WSPreview.PreviewHandler.PreviewHandlerFramework;
-//using ScintillaNET;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Folding;
+using ICSharpCode.AvalonEdit.Highlighting;
 
 namespace WSPreview.PreviewHandler.Controls
 {
@@ -10,29 +16,31 @@ namespace WSPreview.PreviewHandler.Controls
     {
 #region fields
 
-        //private Scintilla _scintilla;
+        private TextEditor _textEditor;
+        private ElementHost _hosting;
+
 
 #endregion
 
         public SourceViewer()
         {
             InitializeComponent();
-            //_scintilla = new Scintilla();
-            //_scintilla.Dock = DockStyle.Fill;
-            //Controls.Add(_scintilla);
+            _textEditor = new TextEditor();
+            _hosting = new ElementHost();
+            _hosting.Dock = DockStyle.Fill;
+            _hosting.Child = _textEditor;
+            Controls.Add(_hosting);
         }
 
         #region public
 
         public void LoadFile(string filename)
         {
-            //if (_scintilla == null)
-            //    return;
-            //_scintilla.UndoRedo.EmptyUndoBuffer();
-            //_scintilla.Modified = false;
-            //ConfigureScintilla();
-            //SetLanguage(filename);
-            //_scintilla.Text = File.ReadAllText(filename);
+            if (_textEditor == null || string.IsNullOrEmpty(filename))
+                return;
+            _textEditor.Load(filename);
+            _textEditor.SyntaxHighlighting =
+                HighlightingManager.Instance.GetDefinitionByExtension(Path.GetExtension(filename));
         }
 
         public void LoadFile(Stream stream)
@@ -41,65 +49,12 @@ namespace WSPreview.PreviewHandler.Controls
 
         public void Clear()
         {
-            //if (_scintilla != null)
-            //{
-            //    _scintilla.Text = string.Empty;
-            //}
+            if (_textEditor == null)
+                return;
+            _textEditor.Clear();
         }
 
         #endregion
-
-        #region private
-
-        private void ConfigureScintilla()
-        {
-        //    if(_scintilla == null)
-        //        return;
-        //    _scintilla.Margins.Margin0.Width = 35;
-        }
-
-        private void SetLanguage(string filename)
-        {
-            //if (_scintilla == null)
-            //    return;
-            //string ext = Path.GetExtension(filename).ToLower();
-            //ext = ext.Substring(1);
-            //_scintilla.ConfigurationManager.Language = "";
-            //switch(ext)
-            //{
-            //    case "cpp":
-            //    case "c":
-            //    case "h":
-            //    case "hpp":
-            //        _scintilla.ConfigurationManager.Language = "cpp";
-            //        break;
-            //    case "java":
-            //    case "js":
-            //        _scintilla.ConfigurationManager.Language = "js";
-            //        break;
-            //    case "bat":
-            //        _scintilla.ConfigurationManager.Language = "Batch";
-            //        break;
-            //    case "asm":
-            //        _scintilla.ConfigurationManager.Language = "Assembly";
-            //        break;
-            //    case "css":
-            //        _scintilla.ConfigurationManager.Language = "CSS";
-            //        break;
-            //    case "cs":
-            //        _scintilla.ConfigurationManager.Language = "cs";
-            //        break;
-            //    case "vb":
-            //        _scintilla.ConfigurationManager.Language = "vb";
-            //        break;
-            //}
-
-            
-        }
-
-
-        #endregion
-
 
     }
 }
