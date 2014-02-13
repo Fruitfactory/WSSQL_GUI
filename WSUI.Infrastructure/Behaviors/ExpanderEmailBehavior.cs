9348d158faef5a05f69110ec589af3939c5fae8d
yariki <iyariki.ya@gmail.com>
2014-02-13 23:58:39 +0200

    updated: - fixed click on first email on thread;

---
+++ WSUI.Infrastructure/Behaviors/ExpanderEmailBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using WSUI.Infrastructure.Helpers.Extensions;
+
+namespace WSUI.Infrastructure.Behaviors
+{
+    public class ExpanderEmailBehavior : Behavior<Expander>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            
+            AssociatedObject.PreviewMouseLeftButtonDown += AssociatedObjectOnPreviewMouseLeftButtonDown;
+        }
+
+        private void AssociatedObjectOnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            if (mouseButtonEventArgs.Source == null || sender == null)
+                return;
+            if (mouseButtonEventArgs.Source != sender)
+                return;
+
+            var item = AssociatedObject.GetParentCore<ListBoxItem>();
+            if (item != null)
+            {
+                item.IsSelected = false;
+                item.IsSelected = true;
+            }
+            var childListBox = AssociatedObject.GetChildren<ListBox>();
+            if (childListBox != null && childListBox.Any())
+            {
+                childListBox.First().SelectedIndex = -1;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewMouseLeftButtonDown -= AssociatedObjectOnPreviewMouseLeftButtonDown;
+            base.OnDetaching();
+        }
+
+    }
+
+}
