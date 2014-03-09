046eeedd4d676a7347cca3a3f9f49df481497782
yariki <iyariki.ya@gmail.com>
2014-03-09 12:23:25 +0200

    updated: - changed the blocking in the base search rule; - fixed some minor bugs;

--- WSUI.Core/Core/Search/BaseSearchRule.cs
+++ WSUI.Core/Core/Search/BaseSearchRule.cs
@@ -23,268 +23,288 @@ using WSUI.Core.Core.Rules;
 using WSUI.Core.Logger;
 using WSUI.Core.Utils;
 
-namespace WSUI.Core.Core.Search 
+namespace WSUI.Core.Core.Search
 {
-	public abstract class BaseSearchRule<T> : ISearch, ISearchRule, IRuleQueryGenerator where T : class, new()
-	{
+    public abstract class BaseSearchRule<T> : ISearch, ISearchRule, IRuleQueryGenerator where T : class, new()
+    {
         #region [needs private]
 
-	    private const string ConnectionString = "Provider=Search.CollatorDSO;Extended Properties=\"Application=Windows\"";
-	    private Thread _ruleThread;
+        private const string ConnectionString = "Provider=Search.CollatorDSO;Extended Properties=\"Application=Windows\"";
+        private Thread _ruleThread;
         private IQueryReader _reader;
-	    private volatile bool _isSearching = false;
+        private volatile bool _isSearching = false;
         #endregion
 
         #region [needs protected]
 
-	    protected volatile bool IsInterupt = false;
+        protected volatile bool IsInterupt = false;
         protected IList<T> Result;
         protected bool NeedInterrup;
         protected DateTime LastDate;
         protected string Query;
         protected AutoResetEvent Event;
         protected int TopQueryResult = 100;
-	    protected int CountFirstProcess = 35;
-	    protected int CountSecondProcess = 7;
+        protected int CountFirstProcess = 35;
+        protected int CountSecondProcess = 7;
         protected int CountAdded = 0;
         protected int CountProcess = 0;
-	    protected volatile bool NeedStop = false;
-        protected volatile object Lock =  null;
+        protected volatile bool NeedStop = false;
+        protected volatile object Lock = null;
 
         // results
-	    private TypeResult _typeResult;
-	    private IList<IResultMessage> _listMessage;
+        private TypeResult _typeResult;
+        private IList<IResultMessage> _listMessage;
 
         protected string QueryAnd = " AND \"{0}\"";
 
 
-	    protected string RuleName;
+        protected string RuleName;
 
         #endregion
 
-	    protected BaseSearchRule()
-            :this(null)
-	    {
-	    }
+        protected BaseSearchRule()
+            : this(null)
+        {
+        }
 
-		protected BaseSearchRule(object lockObject)
-		{   
+        protected BaseSearchRule(object lockObject)
+        {
             Event = new AutoResetEvent(false);
             Result = new List<T>();
-		    Lock = lockObject ?? new object();
-		}
+            Lock = lockObject ?? new object();
+        }
 
-		public ISearchResult GetResults()
-		{
+        public ISearchResult GetResults()
+        {
             return new SearchResult(_typeResult, _listMessage.ToList(), (IEnumerable<ISearchObject>)Result);
-		}
+        }
 
-		/// <summary>
-		/// @param ="criteris"
-		/// </summary>
-		/// <param name="criteria"></param>
-		public virtual void SetSearchCriteria(string criteria)
-		{
-		    Query = criteria;
-		}
+        /// <summary>
+        /// @param ="criteris"
+        /// </summary>
+        /// <param name="criteria"></param>
+        public virtual void SetSearchCriteria(string criteria)
+        {
+            Query = criteria;
+        }
 
-		public void Search()
-		{
+        public void Search()
+        {
             if (_isSearching)
             {
                 Event.Set();
                 return;
             }
-		    InitBeforeSearching();
-            _ruleThread = new Thread(DoQuery) {Priority = ThreadPriority.Highest};
-		    _ruleThread.Start();
-		}
+            InitBeforeSearching();
+            _ruleThread = new Thread(DoQuery) { Priority = ThreadPriority.Highest };
+            _ruleThread.Start();
+        }
 
-		public virtual void Stop()
-		{
-		    NeedStop = true;
-		}
+        public virtual void Stop()
+        {
+            NeedStop = true;
+        }
 
-	    protected virtual void DoQuery()
-	    {
-	        OleDbConnection connection = null;
-	        try
-	        {
+        protected virtual void DoQuery()
+        {
+            OleDbConnection connection = null;
+            try
+            {
                 _isSearching = true;
-	            string query = QueryGenerator.Instance.GenerateQuery(typeof (T), Query, TopQueryResult, this, false);
-	            if (string.IsNullOrEmpty(query))
+                string query = QueryGenerator.Instance.GenerateQuery(typeof(T), Query, TopQueryResult, this, false);
+                if (string.IsNullOrEmpty(query))
                     throw new ArgumentNullException("Query is null or empty");
-                WSSqlLogger.Instance.LogInfo("Query<{0}>: {1}",typeof(T).Name, query);
+                WSSqlLogger.Instance.LogInfo("Query<{0}>: {1}", typeof(T).Name, query);
                 Stopwatch watch = null;
-	            lock (Lock)
-	            {
-	                connection = new OleDbConnection(ConnectionString);
-	                var cmd = new OleDbCommand(query, connection);
-	                cmd.CommandTimeout = 0;
-	                try
-	                {
-	                    watch = new Stopwatch();
-	                    watch.Start();
-	                    connection.Open();
-	                    watch.Stop();
-	                    WSSqlLogger.Instance.LogInfo("ConnectionOpen<{0}>: {1}", typeof (T).Name, watch.ElapsedMilliseconds);
+                //lock (Lock)
+                {
+                    connection = new OleDbConnection(ConnectionString);
+                    var cmd = new OleDbCommand(query, connection);
+                    cmd.CommandTimeout = 0;
+                    try
+                    {
+                        watch = new Stopwatch();
+                        watch.Start();
+                        connection.Open();
+                        watch.Stop();
+                        WSSqlLogger.Instance.LogInfo("ConnectionOpen<{0}>: {1}", typeof(T).Name, watch.ElapsedMilliseconds);
 
-	                    (watch = new Stopwatch()).Start();
-	                    using (var dataReader = cmd.ExecuteReader())
-	                    {
-                            watch.Stop();
-	                        WSSqlLogger.Instance.LogInfo("dataReader<{0}> Elapsed: {1}", typeof (T).Name,
-                                watch.ElapsedMilliseconds);
+                        OleDbDataReader dataReader = null;
+                        try
+                        {
 
-                            (watch = new Stopwatch()).Start(); 
-                            while (dataReader.Read())
-	                        {
-	                            try
-	                            {
-	                                ReadData(dataReader);
-	                                if (IsInterupt || NeedStop)
-	                                    break;
-	                            }
-	                            catch (Exception ex)
-	                            {
-	                                WSSqlLogger.Instance.LogError("{0}<{1}>: {2}", "DoQuery MainCycle", typeof (T).Name,
-	                                    ex.Message);
-	                            }
-	                        }
-	                        watch.Stop();
-	                        WSSqlLogger.Instance.LogInfo("ReadData<{0}>: {1}", typeof (T).Name, watch.ElapsedMilliseconds);
-	                    }
-	                }
-	                catch (OleDbException oleDbException)
-	                {
-	                    WSSqlLogger.Instance.LogError("{0}<{1}>: {2}", "DoQuery", typeof (T).Name, oleDbException.Message);
-	                }
-	            }
-	            // additional process
-	            if (!NeedStop)
-	            {
-                    watch = new Stopwatch(); 
+
+                            (watch = new Stopwatch()).Start();
+                            lock(Lock)
+                                dataReader = cmd.ExecuteReader();
+                            //using (var dataReader = cmd.ExecuteReader())
+                            {
+                                watch.Stop();
+                                WSSqlLogger.Instance.LogInfo("dataReader<{0}> Elapsed: {1}", typeof(T).Name,
+                                    watch.ElapsedMilliseconds);
+
+                                (watch = new Stopwatch()).Start();
+                                lock (Lock)
+                                    while (dataReader.Read())
+                                    {
+                                        try
+                                        {
+                                            ReadData(dataReader);
+                                            if (IsInterupt || NeedStop)
+                                                break;
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            WSSqlLogger.Instance.LogError("{0}<{1}>: {2}", "DoQuery MainCycle",
+                                                typeof(T).Name,
+                                                ex.Message);
+                                        }
+                                    }
+                                watch.Stop();
+                                WSSqlLogger.Instance.LogInfo("ReadData<{0}>: {1}", typeof(T).Name,
+                                    watch.ElapsedMilliseconds);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            WSSqlLogger.Instance.LogInfo("ReadData<{0}>: {1}", typeof(T).Name, ex.Message);
+                        }
+                        finally
+                        {
+                            if (dataReader != null)
+                                dataReader.Close();
+                        }
+                    }
+                    catch (OleDbException oleDbException)
+                    {
+                        WSSqlLogger.Instance.LogError("{0}<{1}>: {2}", "DoQuery", typeof(T).Name, oleDbException.Message);
+                    }
+                }
+                // additional process
+                if (!NeedStop)
+                {
+                    watch = new Stopwatch();
                     watch.Start();
-	                ProcessResult();
-	                _typeResult = TypeResult.Ok;
+                    ProcessResult();
+                    _typeResult = TypeResult.Ok;
                     watch.Stop();
                     WSSqlLogger.Instance.LogInfo("ProcessResult<{0}>: {1}", typeof(T).Name, watch.ElapsedMilliseconds);
-	            }
-	            else
-	            {
-	                _typeResult = TypeResult.Error;
-                    _listMessage.Add(new ResultMessage(){Message = "Rule was stoped"});
-	            }
-                
-	        }
-	        catch (Exception ex)
-	        {
+                }
+                else
+                {
+                    _typeResult = TypeResult.Error;
+                    _listMessage.Add(new ResultMessage() { Message = "Rule was stoped" });
+                }
+
+            }
+            catch (Exception ex)
+            {
                 _typeResult = TypeResult.Error;
-                _listMessage.Add(new ResultMessage(){Message = ex.Message});
-                WSSqlLogger.Instance.LogError("Search <{0}>: {1}",typeof(T).Name, ex.Message);
-	        }
-	        finally
-	        {
-	            if (connection != null && connection.State == ConnectionState.Open)
-	                connection.Close();
+                _listMessage.Add(new ResultMessage() { Message = ex.Message });
+                WSSqlLogger.Instance.LogError("Search <{0}>: {1}", typeof(T).Name, ex.Message);
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                    connection.Close();
                 CountAdded = 0;
-	            TopQueryResult = CountProcess = CountSecondProcess;
+                TopQueryResult = CountProcess = CountSecondProcess;
                 _isSearching = false;
-	            NeedStop = false;
-	            IsInterupt = false;
-	            Event.Set();
-	        }
-	        
-	    }
+                NeedStop = false;
+                IsInterupt = false;
+                Event.Set();
+            }
 
-	    /// 
-	    /// <param name="reader"></param>
-	    protected virtual void ReadData(IDataReader reader)
-	    {
-	        var result = Reader.ReadResult(reader) as T;
-	        if (result == null)
-	            return;
-	        Result.Add(result);
-	        ProcessCountAdded();
-	    }
+        }
 
-	    protected virtual void ProcessCountAdded()
-	    {
-	        CountAdded++;
-	        if (CountAdded == CountProcess)
-	            IsInterupt = true;
-	    }
+        /// 
+        /// <param name="reader"></param>
+        protected virtual void ReadData(IDataReader reader)
+        {
+            var result = Reader.ReadResult(reader) as T;
+            if (result == null)
+                return;
+            Result.Add(result);
+            ProcessCountAdded();
+        }
 
-	    public AutoResetEvent GetEvent()
-	    {
-	        Event.Reset();
-			return Event;
-		}
+        protected virtual void ProcessCountAdded()
+        {
+            CountAdded++;
+            if (CountAdded == CountProcess)
+                IsInterupt = true;
+        }
 
-		public virtual void Reset()
-		{
+        public AutoResetEvent GetEvent()
+        {
+            Event.Reset();
+            return Event;
+        }
+
+        public virtual void Reset()
+        {
             CountAdded = 0;
             CountProcess = 0;
             TopQueryResult = CountProcess = CountFirstProcess;
-		    Query = string.Empty;
-		    LastDate = GetCurrentDateTime();
+            Query = string.Empty;
+            LastDate = GetCurrentDateTime();
             IsInterupt = false;
             NeedStop = false;
             Result.Clear();
-		}
+        }
 
-	    public bool IsSearching { get { return _isSearching; }  }
-	    public int Priority { get; protected set; }
-	    public RuleObjectType ObjectType { get; protected set; }
+        public bool IsSearching { get { return _isSearching; } }
+        public int Priority { get; protected set; }
+        public RuleObjectType ObjectType { get; protected set; }
 
-	    protected virtual void ProcessResult()
-		{
-            
-		}
+        protected virtual void ProcessResult()
+        {
 
-		public virtual void Init()
-		{
-		    LastDate = GetCurrentDateTime();
-		    TopQueryResult = CountProcess = CountFirstProcess;
-		    CountAdded = 0;
+        }
+
+        public virtual void Init()
+        {
+            LastDate = GetCurrentDateTime();
+            TopQueryResult = CountProcess = CountFirstProcess;
+            CountAdded = 0;
             _typeResult = TypeResult.None;
             _listMessage = new List<IResultMessage>();
-		}
+        }
 
-	    public void SetProcessingRecordCount(int first, int second)
-	    {
+        public void SetProcessingRecordCount(int first, int second)
+        {
             TopQueryResult = CountProcess = CountFirstProcess = first;
-	        CountSecondProcess = second;
-	    }
+            CountSecondProcess = second;
+        }
 
-	    public string GenerateWherePart(IList<IRule> listCriteriaRules)
-	    {
-	        return OnGenerateWherePart(listCriteriaRules);
-	    }
+        public string GenerateWherePart(IList<IRule> listCriteriaRules)
+        {
+            return OnGenerateWherePart(listCriteriaRules);
+        }
 
-	    protected virtual string OnGenerateWherePart(IList<IRule> listCriterisRules)
-	    {
-	        return string.Empty;
-	    }
+        protected virtual string OnGenerateWherePart(IList<IRule> listCriterisRules)
+        {
+            return string.Empty;
+        }
 
         protected IQueryReader Reader
-	    {
-            get { return _reader ?? (_reader = QueryReader<T>.CreateNewReader<T>(FieldCash.Instance.GetFields(typeof(T),false))); }	        
-	    }
+        {
+            get { return _reader ?? (_reader = QueryReader<T>.CreateNewReader<T>(FieldCash.Instance.GetFields(typeof(T), false))); }
+        }
 
-	    protected DateTime GetCurrentDateTime()
-	    {
-	        return DateTime.Now.AddDays(1);
-	    }
+        protected DateTime GetCurrentDateTime()
+        {
+            return DateTime.Now.AddDays(1);
+        }
 
         protected string FormatDate(ref DateTime date)
         {
             return date.ToString("yyyy/MM/dd hh:mm:ss").Replace('.', '/');
         }
 
-	    protected Tuple<string,List<string>> GetProcessingSearchCriteria(IList<IRule> listRuleCriteriasRules)
-	    {
-	        var tempCriteria = Query;
+        protected Tuple<string, List<string>> GetProcessingSearchCriteria(IList<IRule> listRuleCriteriasRules)
+        {
+            var tempCriteria = Query;
             var andClause = string.Empty;
             var listW = new List<string>();
 
@@ -294,30 +314,30 @@ namespace WSUI.Core.Core.Search
                 tempCriteria = rule.ClearCriteriaAccordingRule(tempCriteria);
             }
 
-	        if (listW.Count > 1)
-	        {
-	            StringBuilder temp = new StringBuilder();
-	            temp.Append(string.Format("'\"{0}\"", listW[0]));
-	            for (int i = 1; i < listW.Count; i++)
-	            {
-	                temp.Append(string.Format(QueryAnd, listW[i]));
-	            }
-	            andClause = temp.ToString() + "'";
-	        }
-	        else
-	        {
+            if (listW.Count > 1)
+            {
+                StringBuilder temp = new StringBuilder();
+                temp.Append(string.Format("'\"{0}\"", listW[0]));
+                for (int i = 1; i < listW.Count; i++)
+                {
+                    temp.Append(string.Format(QueryAnd, listW[i]));
+                }
+                andClause = temp.ToString() + "'";
+            }
+            else
+            {
                 andClause = string.Format("'\"{0}\"'", listW[0]);
-	        }
-	        return new Tuple<string, List<string>>(andClause,listW.ToList());
-	    }
+            }
+            return new Tuple<string, List<string>>(andClause, listW.ToList());
+        }
 
-	    protected virtual void InitBeforeSearching()
-	    {
-	        _typeResult = TypeResult.None;
-	        _listMessage.Clear();
+        protected virtual void InitBeforeSearching()
+        {
+            _typeResult = TypeResult.None;
+            _listMessage.Clear();
             Result.Clear();
-	    }
+        }
 
-	}//end BaseSearchRule
+    }//end BaseSearchRule
 
 }//end namespace Search
