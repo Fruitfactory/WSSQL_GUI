0c6a760d8715a61b535655ef6f052ba5b4d046ea
yariki <iyariki.ya@gmail.com>
2015-09-12 15:33:53 +0300

    revert to user tracking by default

--- WSUI.Core/Data/ElasticSearch/OFRiverMeta.cs
+++ WSUI.Core/Data/ElasticSearch/OFRiverMeta.cs
@@ -68,15 +68,19 @@ namespace OF.Core.Data.ElasticSearch
             var set = JsonConvert.SerializeObject(idlesettings);
             Settings = set;
 #else
-            //ScheduleType = RiverSchedule.EveryNightOrIdle;
-            //var idlesettings = new {idle_time = 120};
-            //var set = JsonConvert.SerializeObject(idlesettings);
-            //Settings = set;
+            // user tracking
 
-            ScheduleType = RiverSchedule.EveryHours;
-            var idlesettings = new { hour_period = 1 };
+            ScheduleType = RiverSchedule.EveryNightOrIdle;
+            var idlesettings = new { idle_time = 120 };
             var set = JsonConvert.SerializeObject(idlesettings);
             Settings = set;
+
+            //period
+
+            //ScheduleType = RiverSchedule.EveryHours;
+            //var idlesettings = new { hour_period = 1 };
+            //var set = JsonConvert.SerializeObject(idlesettings);
+            //Settings = set;
 
 #endif
 
