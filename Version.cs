008142e2bc2ec8314feaa03c4cef6299d34062de
yariki <iyariki.ya@gmail.com>
2016-02-25 21:08:15 +0200

    version for aervice_app_read_all

--- Version.cs
+++ Version.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyVersion("1.0.2.909")]
-[assembly: AssemblyFileVersion("1.0.2.909")]
+[assembly: AssemblyVersion("1.0.2.918")]
+[assembly: AssemblyFileVersion("1.0.2.918")]
