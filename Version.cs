76c299b00837f24529e484392ac5f7177381c8bf
yariki <iyariki.ya@gmail.com>
2016-07-19 20:54:27 +0300

    new version

--- Version.cs
+++ Version.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyVersion("1.0.3.1058")]
-[assembly: AssemblyFileVersion("1.0.3.1058")]
+[assembly: AssemblyVersion("1.0.3.1063")]
+[assembly: AssemblyFileVersion("1.0.3.1063")]
