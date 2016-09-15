987163e795c798ae5658f00c48961b1e55ce8656
Yariki <iyariki.ya@gmail.com>
2016-09-15 22:01:15 +0300

    new vrsion for testing

--- Version.cs
+++ Version.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyVersion("1.0.2.1110")]
-[assembly: AssemblyFileVersion("1.0.2.1110")]
+[assembly: AssemblyVersion("1.0.2.1117")]
+[assembly: AssemblyFileVersion("1.0.2.1117")]
