531c6271e4652458953b14065e5992d64bdcba44
Yariki <iyariki.ya@gmail.com>
2016-09-20 21:54:39 +0300

    release 1.0.3.1127

--- Version.cs
+++ Version.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyVersion("1.0.2.1117")]
-[assembly: AssemblyFileVersion("1.0.2.1117")]
+[assembly: AssemblyVersion("1.0.3.1127")]
+[assembly: AssemblyFileVersion("1.0.3.1127")]
