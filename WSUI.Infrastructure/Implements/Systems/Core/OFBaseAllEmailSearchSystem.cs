8633658ace8c7df8072394fcba5bcf6feb28518e
Yariki <iyariki.ya@gmail.com>
2017-02-17 20:06:42 +0200

    fix Mond Gaffar

--- WSUI.Infrastructure/Implements/Systems/Core/OFBaseAllEmailSearchSystem.cs
+++ WSUI.Infrastructure/Implements/Systems/Core/OFBaseAllEmailSearchSystem.cs
@@ -17,57 +17,6 @@ namespace OF.Infrastructure.Implements.Systems.Core
             ProcessEmails(rules.Where(r => r.ObjectType == RuleObjectType.Email && r is IEmailSearchRule).OfType<IEmailSearchRule>());
         }
 
-
-        //protected override void DoSearch()
-        //{
-        //    try
-        //    {
-
-        //        var watch = new Stopwatch();
-        //        watch.Start();
-
-        //        var rules = GetRules();
-
-        //        foreach (var rule in rules.OrderBy(r => r.Priority))
-        //        {
-        //            var ev = rule.GetEvent();
-        //            rule.Search();
-        //            ev.WaitOne();
-        //        }
-        //        watch.Stop();
-        //        OFLogger.Instance.LogInfo("------------------- searching is DONE!!!!--------------------- {0} ms", watch.ElapsedMilliseconds);
-
-        //        if (_needStop)
-        //        {
-        //            RaiseSearchStopped();
-        //            _IsSearching = false;
-        //            OFLogger.Instance.LogError("Searching was stoped");
-        //            return;
-        //        }
-        //        ProcessData();
-        //        var items = IsAdvancedMode
-        //            ? rules.Where(item => item.IncludedInAdvancedMode).OrderBy(i => i.Priority)
-        //            : rules.OrderBy(i => i.Priority);
-        //        foreach (var item in items)
-        //        {
-        //            var result = (item as ISearchRule).GetResults();
-        //            if (result == null)
-        //                continue;
-        //            var itemResult = new SystemSearchResult(item.Priority, result.OperationResult, item.ObjectType);
-        //            InternalResult.Add(itemResult);
-        //        }
-        //    }
-        //    catch (System.Exception ex)
-        //    {
-        //        OFLogger.Instance.LogError("{0}", ex.ToString());
-        //    }
-        //    finally
-        //    {
-        //        _IsSearching = false;
-        //        RaiseSearchFinished();
-        //    }
-        //}
-
         private void ProcessEmails(IEnumerable<IEmailSearchRule> emailRules)
         {
             if (emailRules == null || !emailRules.Any())
