da70040f9a749f9243a644623d9ebc598d99f478
Yariki <iyariki.ya@gmail.com>
2017-08-15 20:33:21 -0700

    build new version for Vincent.

--- Version.cs
+++ Version.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyVersion("1.0.3.1407")]
-[assembly: AssemblyFileVersion("1.0.3.1407")]
+[assembly: AssemblyVersion("1.0.3.1456")]
+[assembly: AssemblyFileVersion("1.0.3.1456")]
