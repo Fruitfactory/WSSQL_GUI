b836935137591d4757a25789d5543769ecdc75bf
Yariki <iyariki.ya@gmail.com>
2017-08-23 21:54:13 -0700

    new version 1.0.3.1465

--- Version.cs
+++ Version.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyVersion("1.0.3.1464")]
-[assembly: AssemblyFileVersion("1.0.3.1464")]
+[assembly: AssemblyVersion("1.0.3.1465")]
+[assembly: AssemblyFileVersion("1.0.3.1465")]
